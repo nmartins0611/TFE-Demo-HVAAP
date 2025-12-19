b40a5fc8771a85bab72174dec6548efe98d7d1ce
Nuno Martins <nmartins@redhat.com>
2025-12-19 17:27:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ terraform {
       source  = "hashicorp/tls"
       version = "~> 4.0"
     }
-    aap = {
-      source  = "ansible/aap"
-      version = "~> 1.0"
+   ## aap = {
+   ##   source  = "ansible/aap"
+   ##   version = "~> 1.0"
     }
   }
 }
